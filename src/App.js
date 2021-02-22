83ef9db89f3aec14e3ad8beabf0ba3a93b3a03b8
ohartwig <45525308+otwig@users.noreply.github.com>
2021-02-22 16:10:50 -0500

    fixed nav bar on all pages, added a backdrop to the main page menu

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
         <header><Link to="/"><Header /></Link></header>
       </div>
 
+  
+
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/Site" component={Site} />
