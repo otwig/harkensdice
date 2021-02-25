4a9c7c2769b26ec94d5a25138d136478611976ad
ohartwig <45525308+otwig@users.noreply.github.com>
2021-02-24 23:20:07 -0500

    added all front photos to gallery

--- src/components/siteComps/Gallery.js
+++ src/components/siteComps/Gallery.js
@@ -19,7 +19,7 @@ export default function Gallery() {
         <div className='diceLarge'>
           <img src='/images/astralwinds/IMG_6808.jpg' />
         </ div>
-        <div className='smallGallery'>
+        {/* <div className='smallGallery'>
           <div className='diceSmall'>
             <img src='/images/astralwinds/IMG_6800.jpg' />
           </ div>
@@ -29,16 +29,33 @@ export default function Gallery() {
           <div className='diceSmall'>
             <img src='/images/astralwinds/IMG_6822.jpg' />
           </ div>
-        </div>
+        </div> */}
       </div>
       <hr />
 
       <div className='largeGallery'>
-        <h3>Astral Winds</h3>
+        <h3>Midnight Blue Jelly</h3>
         <div className='diceLarge'>
-          <img src='/images/astralwinds/IMG_6808.jpg' />
+          <img src='/images/blue jelly/IMG_7863.jpg' />
         </ div>
-        <div className='smallGallery'>
+        {/* <div className='smallGallery'>
+    <div className='diceSmall'>
+      <img src='/images/astralwinds/IMG_6800.jpg' />
+    </ div>
+    <div className='diceSmall'>
+      <img src='/images/astralwinds/IMG_6820.jpg' />
+    </ div>
+    <div className='diceSmall'>
+      <img src='/images/astralwinds/IMG_6822.jpg' />
+</ div> */}
+      </div>
+
+      <div className='largeGallery'>
+        <h3>Longstrider</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_6910.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
           <div className='diceSmall'>
             <img src='/images/astralwinds/IMG_6800.jpg' />
           </ div>
@@ -47,11 +64,280 @@ export default function Gallery() {
           </ div>
           <div className='diceSmall'>
             <img src='/images/astralwinds/IMG_6822.jpg' />
-          </ div>
-        </div>
+      </ div> */}
       </div>
       <hr />
-      
+
+      <div className='largeGallery'>
+        <h3>Eldritch Vibes</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_6931.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Eldritch Vibes in Orange</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_6878.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+
+      <div className='largeGallery'>
+        <h3>Eldritch Lime-A-Rita Knight</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_7017.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Pact of the Tequila Sunrise</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_7024.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Curacao Coast</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_7026.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Eldritch Vibes in Yellow with Silver</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_7058.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Rainbow Smoke</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_7066.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Regal</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_7922.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Regal 2</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_6946.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Eladrin Riches</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_6880.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Purple Spectre</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_8005.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Green Plume</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_8089.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Blue Spectre</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_8121.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Alcehmist Fire</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_8159.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+
+<div className='largeGallery'>
+        <h3>Eldritch Vibes in Black Ink</h3>
+        <div className='diceLarge'>
+          <img src='/images/IMG_8059.jpg' />
+        </ div>
+        {/* <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+      </ div> */}
+      </div>
+      <hr />
+      </div>
+      <hr />
     </div>
   )
 }
