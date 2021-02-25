8974ec8189d2575c8d5ab5c2280fee19769c4258
ohartwig <45525308+otwig@users.noreply.github.com>
2021-02-24 22:34:56 -0500

    gave up on carousel, stored it in galleryslides.js. created a stop-gap in divs in gallery.js

--- src/components/siteComps/Gallery.js
+++ src/components/siteComps/Gallery.js
@@ -2,14 +2,56 @@ import React from 'react'
 import '../css/Gallery.css'
 import SiteHeader from './SiteHeader.js'
 import Home from '../Home.js'
-
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function Gallery() {
   return (
     <div className='gallery'>
-      <div className='secretMenu'><Home /><SiteHeader /></div>
-      <img src="/images/dice.png" alt="" />
+      <div className='secretMenu'>
+        <Home />
+        <SiteHeader />
+      </div>
+
+      <h1>Dice Gallery</h1>
+
+      <div className='largeGallery'>
+        <h3>Astral Winds</h3>
+        <div className='diceLarge'>
+          <img src='/images/astralwinds/IMG_6808.jpg' />
+        </ div>
+        <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+          </ div>
+        </div>
+      </div>
+      <hr />
+
+      <div className='largeGallery'>
+        <h3>Astral Winds</h3>
+        <div className='diceLarge'>
+          <img src='/images/astralwinds/IMG_6808.jpg' />
+        </ div>
+        <div className='smallGallery'>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6800.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6820.jpg' />
+          </ div>
+          <div className='diceSmall'>
+            <img src='/images/astralwinds/IMG_6822.jpg' />
+          </ div>
+        </div>
+      </div>
+      <hr />
+      
     </div>
   )
 }
