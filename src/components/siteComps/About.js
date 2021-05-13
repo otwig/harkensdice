b85921f3adeec3cbf02723cd6bb04e73e7de290c
ohartwig <45525308+otwig@users.noreply.github.com>
2021-05-12 21:54:24 -0400

    changed links from ko-fi to tiktok. updated server.js to include port listening message

--- src/components/siteComps/About.js
+++ src/components/siteComps/About.js
@@ -7,15 +7,14 @@ import { Switch, Route, Link } from 'react-router-dom'
 export default function About() {
   return (
     <div className='about'>
-       <Home />
+      <Home />
       <SiteHeader />
 
       <div className="text">
-        <h3>An about title title title </h3>
+        <h3>About Harkens Dice</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et odio pellentesque diam volutpat commodo sed. Leo integer malesuada nunc vel risus commodo viverra maecenas. Pretium lectus quam id leo in. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Felis eget velit aliquet sagittis id. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Vel pharetra vel turpis nunc eget lorem. Turpis in eu mi bibendum neque. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Fermentum leo vel orci porta non. Non curabitur gravida arcu ac. Justo nec ultrices dui sapien eget mi proin sed libero. Non nisi est sit amet facilisis.
-
-In vitae turpis massa sed elementum. Bibendum ut tristique et egestas quis ipsum. Id consectetur purus ut faucibus pulvinar elementum integer enim neque. Adipiscing tristique risus nec feugiat. Vivamus arcu felis bibendum ut tristique et. Interdum consectetur libero id faucibus nisl. Risus pretium quam vulputate dignissim. Scelerisque felis imperdiet proin fermentum leo. Pellentesque diam volutpat commodo sed egestas egestas. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Semper eget duis at tellus at urna condimentum mattis pellentesque. Pretium viverra suspendisse potenti nullam ac tortor vitae. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi. Sapien eget mi proin sed libero enim sed faucibus turpis. Id semper risus in hendrerit gravida rutrum quisque non tellus. Mauris augue neque gravida in fermentum. Vestibulum lectus mauris ultrices eros in cursus. Venenatis lectus magna fringilla urna porttitor. Euismod lacinia at quis risus sed vulputate odio. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras.</p>
+          Harkens Dice is located in Vermont, USA. It is 
+        </p>
       </div>
     </div>
   )
