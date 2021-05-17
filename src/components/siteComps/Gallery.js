678d5017cdedb8df0057398b9bd001973eb23a2a
ohartwig <45525308+otwig@users.noreply.github.com>
2021-05-17 18:30:37 -0400

    cleanup of Gallery.js

--- src/components/siteComps/Gallery.js
+++ src/components/siteComps/Gallery.js
@@ -13,23 +13,13 @@ export default function Gallery() {
       <div className="Text">
         <h3>Dice Gallery</h3>
       </div>
+      
       <div className="galleries">
         <div className='largeGallery'>
           <h4>Astral Winds</h4>
           <div className='diceLarge'>
             <img src='/images/astralwinds/IMG_6808.jpg' />
           </ div>
-          {/* <div className='smallGallery'>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6800.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6820.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6822.jpg' />
-          </ div>
-        </div> */}
         </div>
 
 
@@ -38,16 +28,6 @@ export default function Gallery() {
           <div className='diceLarge'>
             <img src='/images/blue jelly/IMG_7863.jpg' />
           </ div>
-          {/* <div className='smallGallery'>
-    <div className='diceSmall'>
-      <img src='/images/astralwinds/IMG_6800.jpg' />
-    </ div>
-    <div className='diceSmall'>
-      <img src='/images/astralwinds/IMG_6820.jpg' />
-    </ div>
-    <div className='diceSmall'>
-      <img src='/images/astralwinds/IMG_6822.jpg' />
-</ div> */}
         </div>
 
         <div className='largeGallery'>
@@ -55,16 +35,6 @@ export default function Gallery() {
           <div className='diceLarge'>
             <img src='/images/IMG_7024.jpg' />
           </ div>
-          {/* <div className='smallGallery'>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6800.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6820.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6822.jpg' />
-      </ div> */}
         </div>
 
 
@@ -73,16 +43,6 @@ export default function Gallery() {
           <div className='diceLarge'>
             <img src='/images/IMG_7026.jpg' />
           </ div>
-          {/* <div className='smallGallery'>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6800.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6820.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6822.jpg' />
-      </ div> */}
         </div>
 
         <div className='largeGallery'>
@@ -90,16 +50,6 @@ export default function Gallery() {
           <div className='diceLarge'>
             <img src='/images/IMG_6880.jpg' />
           </ div>
-          {/* <div className='smallGallery'>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6800.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6820.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6822.jpg' />
-      </ div> */}
         </div>
 
 
@@ -108,16 +58,6 @@ export default function Gallery() {
           <div className='diceLarge'>
             <img src='/images/IMG_8089.jpg' />
           </ div>
-          {/* <div className='smallGallery'>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6800.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6820.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6822.jpg' />
-      </ div> */}
         </div>
 
         <div className='largeGallery'>
@@ -125,32 +65,13 @@ export default function Gallery() {
           <div className='diceLarge'>
             <img src='/images/IMG_8159.jpg' />
           </ div>
-          {/* <div className='smallGallery'>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6800.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6820.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6822.jpg' />
-      </ div> */}
         </div>
+
         <div className='largeGallery'>
           <h4>Eldritch Vibes in Black Ink</h4>
           <div className='diceLarge'>
             <img src='/images/IMG_8059.jpg' />
           </ div>
-          {/* <div className='smallGallery'>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6800.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6820.jpg' />
-          </ div>
-          <div className='diceSmall'>
-            <img src='/images/astralwinds/IMG_6822.jpg' />
-      </ div> */}
         </div>
       </div>
       <Footer />
