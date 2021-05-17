fe0c91fed4fbb6b350dcc968ae46235ee99bf3eb
ohartwig <45525308+otwig@users.noreply.github.com>
2021-05-17 18:43:02 -0400

    gallery.js rework

--- src/components/siteComps/Gallery.js
+++ src/components/siteComps/Gallery.js
@@ -13,65 +13,65 @@ export default function Gallery() {
       <div className="Text">
         <h3>Dice Gallery</h3>
       </div>
-      
+
       <div className="galleries">
         <div className='largeGallery'>
           <h4>Astral Winds</h4>
-          <div className='diceLarge'>
-            <img src='/images/astralwinds/IMG_6808.jpg' />
-          </ div>
+
+          <img src='/images/astralwinds/IMG_6808.jpg' />
+
         </div>
 
 
         <div className='largeGallery'>
           <h4>Midnight Blue Jelly</h4>
-          <div className='diceLarge'>
-            <img src='/images/blue jelly/IMG_7863.jpg' />
-          </ div>
+
+          <img src='/images/blue jelly/IMG_7863.jpg' />
+
         </div>
 
         <div className='largeGallery'>
           <h4>Pact of the Tequila Sunrise</h4>
-          <div className='diceLarge'>
-            <img src='/images/IMG_7024.jpg' />
-          </ div>
+
+          <img src='/images/IMG_7024.jpg' />
+
         </div>
 
 
         <div className='largeGallery'>
           <h4>Curacao Coast</h4>
-          <div className='diceLarge'>
-            <img src='/images/IMG_7026.jpg' />
-          </ div>
+
+          <img src='/images/IMG_7026.jpg' />
+
         </div>
 
         <div className='largeGallery'>
           <h4>Eladrin Riches</h4>
-          <div className='diceLarge'>
-            <img src='/images/IMG_6880.jpg' />
-          </ div>
+
+          <img src='/images/IMG_6880.jpg' />
+
         </div>
 
 
         <div className='largeGallery'>
           <h4>Green Plume</h4>
-          <div className='diceLarge'>
-            <img src='/images/IMG_8089.jpg' />
-          </ div>
+
+          <img src='/images/IMG_8089.jpg' />
+
         </div>
 
         <div className='largeGallery'>
           <h4>Alchemist's Fire</h4>
-          <div className='diceLarge'>
-            <img src='/images/IMG_8159.jpg' />
-          </ div>
+
+          <img src='/images/IMG_8159.jpg' />
+
         </div>
 
         <div className='largeGallery'>
           <h4>Eldritch Vibes in Black Ink</h4>
-          <div className='diceLarge'>
-            <img src='/images/IMG_8059.jpg' />
-          </ div>
+
+          <img src='/images/IMG_8059.jpg' />
+
         </div>
       </div>
       <Footer />
